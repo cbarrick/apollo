9927e3ac5d6060bf646ff5b0b5fb40dd9632a980
Chris Barrick <cbarrick1@gmail.com>
2018-02-06 15:21:32 -0500

    Add LstmConv4d network

--- networks/__init__.py
+++ networks/__init__.py
@@ -3,6 +3,10 @@ from torch.nn import *
 from . import functional
 from . import init
 
+
+# Generic Architectures
+# --------------------------------------------------
+
 from .mlp import MLP
 
 from .alexnet import AlexNet
@@ -12,3 +16,9 @@ from .vgg import Vgg11
 from .vgg import Vgg13
 from .vgg import Vgg16
 from .vgg import Vgg19
+
+
+# Specialized Architectures
+# --------------------------------------------------
+
+from .LstmConv4d import LstmConv4d
