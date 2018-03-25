5cbb8ba558aa29551cf542ef96fab7914bdf7078
Chris Barrick <cbarrick1@gmail.com>
2018-03-24 20:27:54 -0400

    Make open_mb007 use the generated target files

--- apollo/datasets/ga_power.py
+++ apollo/datasets/ga_power.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from pathlib import Path
+import logging
 
 import pandas as pd
 from pandas.errors import EmptyDataError
@@ -7,57 +8,8 @@ from pandas.errors import EmptyDataError
 import xarray as xr
 
 
-# Names of the columns in the MB007 labels.
-MB007_NAMES = (
-    # Index column.
-    # The name 'reftime' joins with the NAM data.
-    'reftime',
-
-    # The next three columns are undocumented.
-    'unknown1', 'unknown2', 'unknown3',
-
-    # Array A
-    'UGA-A-POA-1-IRR',    # POA Irradiance
-    'UGA-A-POA-2-IRR',    # POA Irradiance
-    'UGA-A-POA-3-IRR',    # POA Irradiance
-    'UGA-A-POA-REF-IRR',  # Cell Temp and Irradiance
-
-    # Array B
-    'UGA-B-POA-1-IRR',    # POA Irradiance
-    'UGA-B-POA-2-IRR',    # POA Irradiance
-    'UGA-B-POA-3-IRR',    # POA Irradiance
-    'UGA-B-POA-REF-IRR',  # Cell Temp and Irradiance
-
-    # Array D
-    # NOTE: array D comes before array C
-    'UGA-D-POA-1-IRR',    # POA Irradiance
-    'UGA-D-POA-2-IRR',    # POA Irradiance
-    'UGA-D-POA-3-IRR',    # POA Irradiance
-    'UGA-D-POA-REF-IRR',  # Cell Temp and Irradiance
-
-    # Array C
-    # NOTE: array C comes after array D
-    'UGA-C-POA-1-IRR',    # POA Irradiance
-    'UGA-C-POA-2-IRR',    # POA Irradiance
-    'UGA-C-POA-3-IRR',    # POA Irradiance
-    'UGA-C-POA-REF-IRR',  # Cell Temp and Irradiance
-
-    # Array E
-    'UGA-E-POA-1-IRR',    # POA Irradiance
-    'UGA-E-POA-2-IRR',    # POA Irradiance
-    'UGA-E-POA-3-IRR',    # POA Irradiance
-    'UGA-E-POA-REF-IRR',  # Cell Temp and Irradiance
-
-    # MDAS weather station
-    'UGA-MET01-POA-1-IRR',  # GHI
-    'UGA-MET01-POA-2-IRR',  # GHI
-
-    # SOLYS2
-    'UGA-MET02-GHI-IRR',  # GHI
-    'UGA-MET02-DHI-IRR',  # DHI
-    'UGA-MET02-FIR-IRR',  # DLWIR
-    'UGA-MET02-DNI-IRR',  # DNI
-)
+# Module level logger
+logger = logging.getLogger(__name__)
 
 
 def interval(year=None, month=None, day=None, hour=None, minute=None):
@@ -133,43 +85,27 @@ def interval(year=None, month=None, day=None, hour=None, minute=None):
     return grouper
 
 
-def open_mb007(*cols, data_dir='./data/GA-POWER'):
+def open_mb007(*cols, group=2017, data_dir='./data/GA-POWER'):
     # The data directory contains more than just the mb-007 labels.
     data_dir = Path(data_dir)
-    paths = data_dir.glob('raw/mb-007.*.log.gz')
+    path = data_dir / f'mb-007.{group}.log'
 
-    # All columns must be given by name.
-    # This enforces good style.
-    if len(cols) is 0:
-        cols = list(MB007_NAMES)
-    for c in cols:
-        assert isinstance(c, str)
+    # Ensure reftime is always selected and is a list.
+    if 'reftime' not in cols and 0 not in cols:
+        cols = ['reftime', *cols]
+    else:
+        cols = list(cols)
 
-    # Ensure reftime is always selected.
-    if 'reftime' not in cols:
-        cols = ('reftime', *cols)
-
-    # Read each log into a dataframe.
-    frames = []
-    for path in paths:
-        try:
-            frames.append(
-                pd.read_csv(
-                    path, header=None, index_col='reftime', usecols=cols,
-                    parse_dates=['reftime'], names=MB007_NAMES))
-        except EmptyDataError:
-            continue
-
-    # Combine the dataframes and aggregate by hour.
-    df = pd.concat(frames)
+    # Load the CSV and aggregate by hour.
+    df = pd.read_csv(path, usecols=cols, index_col='reftime')
     df = df.dropna()
-    df = df.sort_index()
+    df.index = pd.to_datetime(df.index)
     df = df.groupby(interval(hour=1)).mean()
 
     # For some reason, the index name isn't set by default.
     df.index.name = 'reftime'
 
-    # For this project, all datasets should be presented as xarray.
+    # In apollo, all datasets should be presented as xarray.
     df = df.to_xarray()
 
     return df
