7f7d9ec82c70031a7d073c2261d4bf2cce8f68b9
Chris Barrick <cbarrick1@gmail.com>
2018-09-23 18:06:15 -0400

    Improve download script
    These changes add protection against memory leaks and add new cli
    arguments making it easier to download between forecasts on a date range
    and adding the ability to use multiple download processes.
    
    The -c/--cache-dir argument has been renamed to -d/--dest.

--- bin/download.py
+++ bin/download.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Download missing NAM forecasts to the cache.
+'''Download NAM forecasts.
 
 This script is intended to be run as a cron job. Note that it imports
 from `apollo.datasets`, so you have to make sure the package is in your
@@ -12,42 +12,79 @@ We run a cron job similar to the following to keep the cache updated:
     date >> cron.log
     python3 -m bin.download -n 4 2>&1| tee -a ./download.log
 '''
-from apollo.datasets.loaders import nam
 
 import argparse
 import logging
+import multiprocessing as mp
 import sys
+from pathlib import Path
 
 import numpy as np
 
+from apollo.datasets.loaders import nam
+
+
+def xrange_inclusive(start, stop, step):
+    '''Like the builtin `range`, but:
+        1. supports arbitrary data types, like datetime64, and
+        2. is an inclusive range
+    '''
+    while start <= stop:
+        yield start
+        start += step
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Download missing NAM forecasts to the cache.')
-    parser.add_argument('-c', '--cache-dir', type=str, default='./data/NAM-NMM', help='Path to the cache.')
-    parser.add_argument('-n', '--count', type=int, default=1, metavar='N', help='Download N datasets, ending at the reference time.')
+    # Note that the `--from` argument is parsed into `args.start`
+    parser = argparse.ArgumentParser(description='Download NAM forecasts between two times, inclusive.')
     parser.add_argument('-x', '--fail-fast', action='store_true', help='Do not retry downloads.')
     parser.add_argument('-k', '--keep-gribs', action='store_true', help='Do not delete grib files.')
-    parser.add_argument('time', nargs='?', default='now', help='The reference time to download.')
+    parser.add_argument('-d', '--dest', type=str, default='./data/NAM-NMM', help='Path to the data cache.')
+    parser.add_argument('-p', '--procs', type=int, default=1, help='Use this many download processes. Defaults to 1.')
+    parser.add_argument('-n', '--count', type=int, help='Download this many forecasts, ending at the reftime.')
+    parser.add_argument('-r', '--from', type=str, dest='start', help='Download multiple forecasts starting from this timestamp.')
+    parser.add_argument('-l', '--log', type=str, default='INFO', help='Set the log level.')
+    parser.add_argument('reftime', nargs='?', default='now', help='The reftime of the forecast as an ISO 8601 timestamp. Defaults to now.')
     args = parser.parse_args()
 
-    logging.basicConfig(format='[{asctime}] {levelname}: {message}', style='{')
-    nam.logger.setLevel('DEBUG')
+    logging.basicConfig(format='[{asctime}] {levelname}: {message}', style='{', level=args.log)
+    nam.logger.setLevel(args.log)
 
-    if args.count < 1:
-        logging.error(f'Count must be greater than 0, got {args.count}')
+    if args.count and args.start:
+        print('The arguments --count/-n and --from/-r are mutually exclusive.')
         sys.exit(1)
 
-    reftime = np.datetime64(args.time, '6h')
+    logging.debug('called with the following options:')
+    for arg, val in vars(args).items():
+        logging.debug(f'  {arg}: {val}')
 
-    for i in range(args.count):
+    step = np.timedelta64(6, 'h')
+    stop = np.datetime64(args.reftime, '6h')
+    if args.start:
+        start = np.datetime64(args.start, '6h')
+    elif args.count:
+        start = stop - (args.count - 1) * step
+    else:
+        start = stop
+
+    logging.info(f'downloading forecasts from {start} to {stop}')
+
+    def download(reftime):
         try:
             nam.open(
                 reftime,
-                cache_dir=args.cache_dir,
+                cache_dir=args.dest,
                 fail_fast=args.fail_fast,
-                keep_gribs=args.keep_gribs)
+                keep_gribs=args.keep_gribs,
+            )
         except Exception as e:
             logging.error(e)
-            logging.error(f'Could not load data from {reftime}')
+            logging.error(f'Could not load data for {reftime}')
 
-        reftime -= np.timedelta64(6, 'h')
+    # The argument `maxtasksperchild` causes the worker process to be destroyed
+    # and restarted after fufilling this many tasks. This is defensive against
+    # resource leaks in the NAM loader which may have existed with older xarray
+    # versions.
+    with mp.Pool(args.procs, maxtasksperchild=1) as pool:
+        reftimes = xrange_inclusive(start, stop, step)
+        pool.map(download, reftimes)
