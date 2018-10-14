398037973b4d42e38940bfa4a3be6b529c7adfc9
Zach Jones <zachydj@gmail.com>
2018-10-14 02:36:17 -0400

    Update CLI to use Predictors

--- apollo/prediction/__main__.py
+++ apollo/prediction/__main__.py
@@ -1,6 +1,6 @@
 import argparse
 import numpy as np
-from apollo.models.SKModel import SKModel
+from apollo.prediction.SKPredictor import SKPredictor
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
 from sklearn.neighbors import KNeighborsRegressor
@@ -9,32 +9,41 @@ from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
 
 MODELS = {
-    'linreg': SKModel('linear_regression', LinearRegression, parameter_grid=None),
-    'svr': SKModel('svr', SVR, {
+    'linreg': LinearRegression(),
+    'svr': SVR(),
+    'knn': KNeighborsRegressor(),
+    'dtree': DecisionTreeRegressor(),
+    'rf': RandomForestRegressor(),
+    'gbt': XGBRegressor()
+}
+
+PARAM_GRIDS = {
+    'linreg': None,
+    'svr': {
         'C': np.arange(1.0, 1.6, 0.2),                  # penalty parameter C of the error term
         'epsilon': np.arange(0.4, 0.8, 0.1),            # width of the no-penalty region
         'kernel': ['rbf', 'sigmoid'],                   # kernel function
         'gamma': [1/1000]                               # kernel coefficient
-    }),
-    'knn': SKModel('knn', KNeighborsRegressor, {
+    },
+    'knn': {
         'n_neighbors': np.arange(3, 15, 2),             # k
         'weights': ['uniform', 'distance'],             # how are neighboring values weighted
-    }),
-    'dtree': SKModel('dtree', DecisionTreeRegressor, {
+    },
+    'dtree': {
         'splitter': ['best', 'random'],                 # splitting criterion
         'max_depth': [None, 10, 20, 30],                # Maximum depth of the tree. None means unbounded.
         'min_impurity_decrease': np.arange(0.15, 0.40, 0.05)
-    }),
-    'rf': SKModel('rf', RandomForestRegressor, {
+    },
+    'rf': {
         'n_estimators': [10, 50, 100, 250],
         'max_depth': [None, 10, 20, 30],                # Maximum depth of the tree. None means unbounded.
         'min_impurity_decrease': np.arange(0.15, 0.40, 0.05)
-    }),
-    'gbt': SKModel('gbt', XGBRegressor, {
+    },
+    'gbt': {
         'learning_rate': np.arange(0.03, 0.07, 0.02),   # learning rate
         'n_estimators': [50, 100, 200, 250],            # number of boosting stages
         'max_depth': [3, 5, 10, 20],                    # Maximum depth of the tree. None means unbounded.
-    }),
+    }
 }
 
 
@@ -49,18 +58,19 @@ def main():
     parser.add_argument('--model', '-m', default='dtree', type=str, choices=list(MODELS.keys()),
                         help='The name of the model that you would like to run.')
 
-    parser.add_argument('--begin_date', '-b', default='2017-01-01 00:00', type=str,
+    parser.add_argument('--start', '-b', default='2017-01-01 00:00', type=str,
                         help='The start date of the dataset that you want to use. '
                              'Any string accepted by numpy\'s datetime64 constructor will work.')
 
-    parser.add_argument('--end_date', '-e', default='2017-12-31 18:00', type=str,
+    parser.add_argument('--stop', '-e', default='2017-12-31 18:00', type=str,
                         help='The end date of the dataset that you want to use. '
                              'Any string accepted by numpy\'s datetime64 constructor will work.')
 
-    parser.add_argument('--target_hour', '-i', default=24, type=int,
-                        help='The prediction hour to target.  Should be an integer between 1 and 36.')
+    parser.add_argument('--target_hour', '-h', default=24, type=int,
+                        help='Generate predictions for each our up to this hour. '
+                             'Should be an integer between 1 and 36.')
 
-    parser.add_argument('--target_var', '-t', default='UGA-C-POA-1-IRR', type=str,
+    parser.add_argument('--target', '-t', default='UGA-C-POA-1-IRR', type=str,
                         help='The variable from GA_POWER to target.')
 
     parser.add_argument('--save_dir', '-s', default='./models', type=str,
@@ -100,35 +110,32 @@ def main():
 
     # every subparser has an action arg specifying which action to perform
     action = args.pop('action')
-    # argparse guarantees that `args.model` will be the key name of one of the models
-    model = MODELS[args.pop('model')]
+    # `args.model` will be the key name of one of the models
+    model_name = args.pop('model')
+    predictor = SKPredictor(name=model_name, estimator=MODELS[model_name], parameter_grid=PARAM_GRIDS[model_name],
+                            target=args['target'], target_hours=np.arange(1, args['target_hour'] + 1))
 
     # do a bit of preprocessing with the tuning argument
     if 'no_tune' in args:
-        dont_tune = args.pop('no_tune')
-        args['tune'] = not dont_tune
+        args['tune'] = not args.pop('no_tune')
 
     if action == 'train':
-        save_path = model.train(**args)
+        save_path = predictor.train(**args)
         print(f'Model trained successfully.  Saved to {save_path}')
+
     elif action == 'evaluate':
-        scores = model.evaluate(**args)
-        # report the mean scores for each metrics
+        scores = predictor.cross_validate(**args)
+        # report the mean scores for each metric
         for key in scores:
             print("Mean %s: %0.4f" % (key, scores[key]))
+
     elif action == 'predict':
-        predictions = model.predict(begin_date=args['begin_date'], end_date=args['end_date'],
-                                    target_hour=args['target_hour'], target_var=args['target_var'],
-                                    save_dir=args['save_dir'])
-
-        summary_path, prediction_path = \
-            model.write_predictions(predictions, begin_date=args['begin_date'], end_date=args['end_date'],
-                                    target_hour=args['target_hour'], target_var=args['target_var'],
-                                    summary_dir=args['summary_dir'], output_dir=args['output_dir'])
-
+        predictions = predictor.predict(**args)
+        summary_path, prediction_path = predictor.write_predictions(predictions, **args)
         print(f'Summary file written to {summary_path}\nPredictions written to {prediction_path}')
+
     else:
-        print(f'ERROR: Action {action} is not defined for model {model}')
+        print(f'ERROR: Action {action} is not defined for model {model_name}')
         parser.print_help()
 
 
