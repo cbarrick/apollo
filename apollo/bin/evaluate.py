56af2988044c8c308b941e207438bf92ad21743e
Zach Jones <zachydj@gmail.com>
2019-03-23 18:12:38 -0400

    Make evaluation CLI tool compatible with new validation API
    Also, reduce the line lengths to be <= 80 columns

--- apollo/bin/evaluate.py
+++ apollo/bin/evaluate.py
@@ -1,14 +1,19 @@
 import argparse
-from sklearn.metrics import mean_absolute_error as mae, mean_squared_error as mse, r2_score as r2
+from math import floor
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_absolute_error as mae, \
+    mean_squared_error as mse, r2_score as r2
+from sklearn.model_selection import TimeSeriesSplit, PredefinedSplit
 
 from apollo.models.base import list_trained_models
 from apollo.models.base import load as load_model
 from apollo.models import *
-from apollo.validation import cross_validate, split_validate
 
 
-def rmse(y_true, y_pred):
-    return mse(y_true, y_pred)**0.5
+# define custom RMSE metric
+def rmse(y_true, y_pred, **kwargs):
+    return mse(y_true, y_pred, **kwargs)**0.5
 
 
 def main():
@@ -22,41 +27,67 @@ def main():
                         help='The name of the saved model to be evaluated.')
 
     parser.add_argument('mode', type=str, choices=('cross_val', 'split'),
-                        help='Validation mode.  k-fold timeseries cross-validation or train-test split.')
+                        help='Validation mode.'
+                             ' K-fold timeseries cross-validation'
+                             ' or train-test split.')
 
-    parser.add_argument('--start', '-b', default='2017-01-01 00:00', type=str,
-                        help='The first reftime in the dataset to be used for training.  '
-                             'Any string accepted by numpy\'s datetime64 constructor will work.')
+    parser.add_argument('--first', '-b', default='2017-01-01 00:00', type=str,
+                        help='The first reftime in the dataset.')
 
-    parser.add_argument('--stop', '-e', default='2017-12-31 18:00', type=str,
-                        help='The final reftime in the dataset to be used for training. '
-                             'Any string accepted by numpy\'s datetime64 constructor will work.')
+    parser.add_argument('--last', '-e', default='2017-12-31 18:00', type=str,
+                        help='The final reftime in the dataset.')
 
     parser.add_argument('--k', '-k', default=5, type=int,
-                        help='Number of folds to use for cross-validation.  Ignored if using `split` mode.')
+                        help='Number of folds to use for cross-validation.'
+                             ' Ignored if using `split` mode.')
 
     parser.add_argument('--split_size', '-p', default=0.25, type=float,
-                        help='Proportion of the dataset to be used for testing.  '
-                             'Ignored if using `cross_val` mode.')
+                        help='Proportion of the dataset to be used for testing.'
+                             ' Ignored if using `cross_val` mode.')
 
+    parser.add_argument('--average', '-a', action='store_true',
+                        help='If set, the evaluations of each target hour will'
+                             ' be reduced to a single value by taking the mean'
+                             ' with uniform weights')
 
     # parse args
     args = parser.parse_args()
     args = vars(args)
 
-    args['split_size'] = max(0, min(args['split_size'], 1))  # squeeze training size between [0, 1]
-
     print('Loading model...')
     model_name = args['name']
     model = load_model(model_name)
 
+    multioutput = 'raw_values'
+    if 'average' in args:
+        multioutput = 'uniform_average'
+
     print('Evaluating...')
+    metrics = (mae, mse, rmse, r2)
     if args['mode'] == 'split':
-        results = split_validate(model, first=args['start'], last=args['stop'], test_size=args['split_size'],
-                                 metrics=(mae, mse, rmse, r2))
+        # use a time-series splitter
+        splitter = TimeSeriesSplit(n_splits=args['k'])
     else:
-        results = cross_validate(model, first=args['start'], last=args['stop'], k=args['k'],
-                                 metrics=(mae, mse, rmse, r2))
+        # create custom splitter with PredefinedSplit
+        test_pct = max(0, min(args['split_size'], 1))
+        first = pd.Timestamp(args['first']).floor(freq='6h')
+        last = pd.Timestamp(args['last']).floor(freq='6h')
+        # total reftimes in the selected dataset
+        reftime_count = (last - first) // pd.Timedelta(6, 'h')
+
+        # create index for PredefinedSplit
+        testing_count = floor(reftime_count*test_pct)
+        training_count = reftime_count - testing_count
+        test_fold = np.concatenate((
+            np.ones(training_count) * -1,   # -1 indicates training set
+            np.zeros(testing_count)         # 0 indicates testing set, 1st fold
+        ))
+        splitter = PredefinedSplit(test_fold)
+
+    results = model.validate(
+        first=args['first'], last=args['last'],
+        splitter=splitter, metrics=metrics,
+        multioutput=multioutput)
 
     print('Results:\n%s' % results)
 
