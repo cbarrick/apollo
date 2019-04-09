08b2ef4d74d6efd31c6218f7278ca30770e8f61f
Zach Jones <zachydj@gmail.com>
2019-04-09 05:58:48 -0400

    Add persistence model to module imports

--- apollo/models/__init__.py
+++ apollo/models/__init__.py
@@ -1,4 +1,5 @@
 # we would like `import apollo.models` to import all instantiable models
+from apollo.models.persistence import *
 from apollo.models.linear import *
 from apollo.models.neighbors import *
 from apollo.models.trees import *
