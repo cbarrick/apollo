8410fdf20e778f93c513f7a3cd32d594b93e8748
Chris Barrick <cbarrick1@gmail.com>
2019-04-14 19:40:31 -0400

    Revamp delete CLI

--- apollo/bin/delete.py
+++ apollo/bin/delete.py
@@ -5,24 +5,20 @@ from apollo.models.base import delete as delete_model
 
 
 def main(argv=None):
-    model_names = list_trained_models()
-
     parser = argparse.ArgumentParser(
-        description='Apollo Model Deleter',
-        argument_default=argparse.SUPPRESS,
+        description='Delete a trained model.',
     )
 
     parser.add_argument(
-        'name',
+        'model',
         type=str,
-        choices=model_names,
-        help='the name of the saved model you wish to delete'
+        metavar='MODEL',
+        help='the name of the model',
     )
 
     args = parser.parse_args(argv)
-    args = vars(args)
 
-    delete_model(args['name'])
+    delete_model(args.model)
 
 
 if __name__ == '__main__':
