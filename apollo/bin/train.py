95d8130a02c118373c2cc7231ef0b3a6e3f5c72b
Chris Barrick <cbarrick1@gmail.com>
2019-04-19 18:32:20 -0400

    Revamp train CLI

--- apollo/bin/train.py
+++ apollo/bin/train.py
@@ -1,80 +1,62 @@
 import argparse
+import logging
 
-from apollo.models.base import save as save_model, list_known_models
-
-
-def _is_abstract(cls):
-    if not hasattr(cls, "__abstractmethods__"):
-        return False  # an ordinary class
-    elif len(cls.__abstractmethods__) == 0:
-        return False  # a concrete implementation of an abstract class
-    else:
-        return True  # an abstract class
-
-
-def parse_kwarg_list(kwarg_list):
-    ''' Parses a list of kwargs formatted like "arg=val"
-    '''
-    return {pair[0]: pair[1] for pair
-            in (kwarg.split('=') for kwarg in kwarg_list)}
+from apollo import timestamps
+from apollo.models.base import save as save_model
+from apollo.models.base import list_known_models
 
 
 def main(argv=None):
-    models = {
-        model.__name__: model
-        for model in list_known_models()
-        if not _is_abstract(model)
-    }
-
     parser = argparse.ArgumentParser(
-        description='Apollo Model Trainer',
-        argument_default=argparse.SUPPRESS,
+        description='Train a new Apollo model.',
     )
 
     parser.add_argument(
         'model',
+        metavar='MODEL',
         type=str,
-        choices=list(models.keys()),
-        help='the type of the model to train',
+        help='the class of the model to train',
     )
 
     parser.add_argument(
-        '--start',
-        '-b',
-        default='2017-01-01T00:00',
-        type=str,
-        help='the first reftime in the training dataset',
-    )
-
-    parser.add_argument(
-        '--stop',
-        '-e',
-        default='2017-12-31T18:00',
-        type=str,
-        help='the final reftime in the training dataset',
-    )
-
-    parser.add_argument(
-        '--kwarg',
+        '--hyper',
+        metavar='KEY=VALUE',
         type=str,
         action='append',
-        help='a hyper-parameter for the model, e.g. `--kwarg arg1=val1`',
+        default=[],
+        help='set a hyper-parameter for the model, may be specified multiple times',
     )
 
-    # parse args
+    selectors = parser.add_mutually_exclusive_group()
+
+    selectors.add_argument(
+        '-r',
+        '--range',
+        nargs=2,
+        metavar=('START', 'STOP'),
+        default=('2017-01-01T00:00', '2017-12-31T18:00'),
+        help='train on all forecast on this range, inclusive'
+    )
+
+    # TODO: Add more selectors to be consistent with other Apollo CLIs.
+    # This requires our models to be more Scikit-learn compatible (#65).
+    # With multiple selectors, we can't set a default for argparse.
+
     args = parser.parse_args(argv)
-    args = vars(args)
 
-    model_classname = args['model']
-    ModelClass = models[model_classname]
+    classes = {model.__name__: model for model in list_known_models()}
 
-    kwarg_list = args['kwarg'] if 'kwarg' in args else list()
-    kwargs = parse_kwarg_list(kwarg_list)
+    ModelClass = classes[args.model]
+    kwargs = dict(pair.split('=') for pair in args.hyper)
+    first = timestamps.utc_timestamp(args.range[0]).floor('6h')
+    last = timestamps.utc_timestamp(args.range[1]).floor('6h')
 
+    logging.info(f'Constructing {args.model} with kwargs: {kwargs}')
     model = ModelClass(**kwargs)
-    model.fit(first=args['start'], last=args['stop'])
+    model.fit(first, last)
+
+    logging.info(f'Saving model with name "{model.name}"')
     save_model(model)
-    print(f'Model saved under name "{model.name}"')
 
 
 if __name__ == '__main__':
