ab560e72cf77bb99aea3d7a96a8f2f839e7ec557
Zach Jones <zachydj@gmail.com>
2019-05-01 02:06:26 -0400

    Update default hyperparams

--- apollo/models/trees.py
+++ apollo/models/trees.py
@@ -17,8 +17,9 @@ class DecisionTree(ScikitModel):
     @property
     def default_hyperparams(self):
         return {
-            'splitter': 'best',
-            'max_depth': 20,
+            'criterion': 'mae',
+            'splitter': 'random',
+            'max_depth': 10,
             'min_impurity_decrease': 0.25
         }
 
@@ -35,9 +36,10 @@ class RandomForest(ScikitModel):
     @property
     def default_hyperparams(self):
         return {
-            'n_estimators': 100,
-            'max_depth': 50,
-            'min_impurity_decrease': 0.30
+            'criterion': 'mae',
+            'n_estimators': 125,
+            'max_depth': 20,
+            'min_impurity_decrease': 0.4
         }
 
 
@@ -53,7 +55,7 @@ class GradientBoostedTrees(ScikitModel):
     @property
     def default_hyperparams(self):
         return {
-            'learning_rate': 0.05,
-            'n_estimators': 200,
-            'max_depth': 20,
+            'learning_rate': 0.06,
+            'n_estimators': 125,
+            'max_depth': 5,
         }
