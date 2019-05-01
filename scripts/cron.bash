8d15d20f8be6ab1332e787104c367e52fb6ad937
Chris Barrick <cbarrick1@gmail.com>
2019-05-01 10:52:13 -0400

    Update cron script

--- scripts/cron.bash
+++ scripts/cron.bash
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# Set the path to the data store
-export APOLLO_DATA='/mnt/data6tb/chris/data'
-
-# Change to the working directory of the project.
-# This makes sure the proper python modules are in scope.
-cd /mnt/data6tb/chris/
-
-# Activate the Anaconda environment.
 source '/opt/conda/etc/profile.d/conda.sh'
-conda activate ./prod_env
+conda activate
 
-# Make sure the most recent 4 forecasts are in the data store.
-python -m apollo.bin.download -n 4 2>&1 | tee -a ./download.log
+export APOLLO_DATA='/apollo-data'
+
+apollo download | tee -a ./download.log
