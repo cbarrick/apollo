772116fc989ee262822ddeef14d92c217f58aa7c
Chris Barrick <cbarrick1@gmail.com>
2019-07-01 14:53:55 -0400

    Remove legacy modeling API

--- apollo/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# we would like `import apollo.models` to import all instantiable models
-from apollo.models.persistence import *
-from apollo.models.linear import *
-from apollo.models.neighbors import *
-from apollo.models.trees import *
-from apollo.models.neural_nets import *
