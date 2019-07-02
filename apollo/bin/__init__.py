6e891aaafd1a2fefc55c1f41dea32e93f45c1217
Chris Barrick <cbarrick1@gmail.com>
2019-07-02 18:01:51 -0400

    Add --debug and --quiet options to root CLI

--- apollo/bin/__init__.py
+++ apollo/bin/__init__.py
@@ -3,23 +3,28 @@ import logging
 from apollo.bin import _cli
 
 
-def setup_logging(level):
+def setup_logging(args):
     '''Setup the logging for the app.
 
     We use module-level loggers everywhere. Most of them are already
     initialized before we get to configure them. So we must reset the
     default log handlers that have already been created.
     '''
+    if args.quiet:
+        level = 'WARN'
+    elif args.debug:
+        level = 'DEBUG'
+    else:
+        level = args.log
+
     # Configure the root logger.
+    log_format = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
     log_handler = logging.StreamHandler()
-    log_handler.setFormatter(logging.Formatter(
-        '[{asctime}] {levelname}: {message}',
-        style='{',
-    ))
+    log_handler.setFormatter(log_format)
     root_logger = logging.getLogger('')
     root_logger.handlers = [log_handler]  # Replace the existing log handlers.
 
-    # Configure all loggers under the `apollo` namespace.
+    # Only set the level for loggers under the `apollo` namespace.
     apollo_logger = logging.getLogger('apollo')
     apollo_logger.setLevel(level)
 
@@ -29,8 +34,21 @@ def main(argv):
         description='the Apollo irradiance forecast system',
     )
 
-    parser.add_argument(
-        '-l',
+    log_options = parser.add_mutually_exclusive_group()
+
+    log_options.add_argument(
+        '--quiet',
+        action='store_true',
+        help='only log error and warning messages (i.e. --log=WARN)'
+    )
+
+    log_options.add_argument(
+        '--debug',
+        action='store_true',
+        help='log debug messages (i.e. --log=DEBUG)'
+    )
+
+    log_options.add_argument(
         '--log',
         metavar='LEVEL',
         type=str,
@@ -40,6 +58,6 @@ def main(argv):
 
     args = parser.parse_args(argv)
 
-    setup_logging(args.log)
+    setup_logging(args)
 
     _cli.execute_subcommand(args)
