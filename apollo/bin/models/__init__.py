fcec77c2f79b91032bc7b51a2e24c63ba3eb7839
Chris Barrick <cbarrick1@gmail.com>
2019-07-09 15:17:29 -0400

    Flatten CLI commands

--- apollo/bin/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from apollo.bin import _cli
-
-def main(argv):
-    parser = _cli.subcommand_parser(
-        description='subcommands for working with models',
-    )
-
-    args = parser.parse_args(argv)
-    _cli.execute_subcommand(args)
