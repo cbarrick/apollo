b82a461f6312f4579f871bf211be44885df2db2e
Chris Barrick <cbarrick1@gmail.com>
2019-08-12 11:09:18 -0400

    Move experiment debug data and remove run_all.sh

--- experiments/00_simple_linear/run_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -euf
-
-apollo --debug train --name linear_v1 -o linear_v1.model ./linear_v1.json ./train.csv
-apollo --debug train --name linear_v2 -o linear_v2.model ./linear_v2.json ./train.csv
-apollo --debug train --name linear_v3 -o linear_v3.model ./linear_v3.json ./train.csv
